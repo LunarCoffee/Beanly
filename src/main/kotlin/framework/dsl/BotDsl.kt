53cdcda5377f7fb070c76280b29d08d44ce53209
Fred G <lunarcoffee.pjc@gmail.com>
2019-04-21 04:08:06 +0000

    Remove redundant property

--- src/main/kotlin/framework/dsl/BotDsl.kt
+++ src/main/kotlin/framework/dsl/BotDsl.kt
@@ -12,22 +12,6 @@ class BotDsl(configPath: String) : Bot(configPath) {
         set(value) {
             jda.presence.activity = value
         }
-
-    var listenerClass: KClass<out ListenerAdapter>? = null
-        set(value) {
-            jda.addEventListener(
-                value
-                    ?.constructors
-                    ?.find {
-                        // Make sure the constructor takes one argument of type [Bot].
-                        it.parameters.run {
-                            size == 1 && get(0).type.classId == Bot::class.classId
-                        }
-                    }
-                    ?.call(this)
-            )
-            field = value
-        }
 }
 
 fun startBot(configPath: String, init: BotDsl.() -> Unit): Bot {
